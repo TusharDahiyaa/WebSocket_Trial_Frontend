f642d1b7e4de465e8b4ebf1e8a9e1dff3c230a3c
Tushar Dahiya <tushardahiya202@gmail.com>
2024-01-30 05:52:52 +0530

    CORS issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import io from "socket.io-client";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 import NewMessage from "./components/NewMessage";
-const socket = io.connect(BASE_URL);
+const socket = io(BASE_URL);
 
 function App() {
   return (
